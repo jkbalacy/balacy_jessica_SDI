b9926f5fe0dacd05183711760983dc3180b8ca51
Jessica Balacy <jkbalacy@fullsail.edu>
2015-01-15 21:51:59 -0500

    Added the second prompt
    Changed variables to calculate the average amount of miles ran each day
    given the miles ran each week (adjusted flowchart to match).
    Calculated the average miles currently running a day.
    Added first arithmetic operator
    Began working on goal miles.

--- SDI_Week2/Balacy_Jessica_Expressions_Assignment/js/expressionsJs.js
+++ SDI_Week2/Balacy_Jessica_Expressions_Assignment/js/expressionsJs.js
@@ -10,9 +10,26 @@ SDI Week #2: Expressions
 var userName = prompt ("Hello!  What is your name?");
 console.log ("The user's name is " +userName+ "."); //Record the user's name into the console.
 
-alert ("Welcome, " +userName+ "! \nLet's calculate how many miles you run in a week.");
+alert ("Welcome, " +userName+ "! \nLet's calculate how many miles you currently run each day.");
 
 //Create an array for the days of the week
 var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-console.log ("There are " +weekDays.length+ " days in a week.")//Record the number of days in the week.
+console.log ("There are " +weekDays.length+ " days in a week."); //Record the number of days in the week.
+
+//Obtain the number of miles the user runs per week
+var weeklyMiles = prompt ("How many miles do you run each week?");
+console.log (userName + " runs " +Math.round(weeklyMiles)+ " miles a week.");
+
+//Calculate the average miles ran a day by dividing the weekly miles by the length of the weekDay array
+var dailyMiles = parseInt(weeklyMiles); //Declare parseInt(dailyMiles) as a variable to perform an arithmetic operator
+alert ("Excellent! \nYou currently run an average of " +Math.round(dailyMiles /= weekDays.length)+ " miles a day.");
+
+console.log (userName + " currently runs an average of " +Math.round(weeklyMiles / weekDays.length)+ " miles a day.");
+
+//Obtain the number of miles the user wants to increase by each week
+var goalMiles = prompt ("Let's make a goal to increase the amount of miles you run. \nHow many more miles would you like to run each week?");
+
+
+
+
 
