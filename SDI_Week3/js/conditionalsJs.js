47b36ffd664599636fa50622b9ce2a92bde8bd86
Jessica Balacy <jkbalacy@fullsail.edu>
2015-01-22 20:29:47 -0500

    Set up files
    Organized files in repository
    Completed conditionals flowchart
    Added prompts
    Added one ternary conditional statement
    Added one logical operator

---
+++ SDI_Week3/js/conditionalsJs.js
@@ -0,0 +1,27 @@
+/* Jessica Balacy
+ January 21, 2015
+ SDI Week #3: Conditionals
+ */
+
+//ask the user for the mileage of his/her car at the time of his/her last oil change.
+var milesBefore = prompt ("Please enter the mileage of your car when your oil was last changed.");
+//check to see if the user left the field blank or entered a text string.
+if (isNaN(milesBefore) || milesBefore ==="") {
+ prompt ("Please input a numerical value.");
+
+}
+
+//ask the uer for his/her current gas mileage.
+var currentMiles = prompt ("Please enter the current mileage of your car.");
+//check to see if the user left the field blank or entered a text string.
+if (isNaN(currentMiles) || currentMiles ==="") {
+ prompt ("Please input a numerical value.");
+
+}
+
+var monthsSince = prompt ("How many months has it been since your last oil change?");
+//check to see if the user left the field blank or entered a text string.
+monthsSince = (isNaN(monthsSince) || monthsSince=="") ? prompt ("Please input a numerical value."): alert ("Thank you!");
+
+
+
