abc7eb939256cfa75ec06449f025b7814d02558f
Jessica Balacy <jkbalacy@fullsail.edu>
2015-01-29 20:53:43 -0500

    Flowchart and File set up
    Set up a new folder for week 4 in repository.  Created flowchart to
    calculate the user’s Basal Metabolic Rate (BMR).

---
+++ SDI_Week4/js/functionsjs.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jessdesuka on 1/29/15.
+ */
+/* Jessica Balacy
+ January 29, 2015
+ SDI Week #4: Functions
+ */
+
+//Calculate the user's Basal Metabolic Rate (BMR) by gathering his/her weight in pounds, height in inches, age, and gender.
+
