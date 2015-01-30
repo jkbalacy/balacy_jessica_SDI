785fc61ae431b0218a1bbe7cddafddb16fed14b8
Jessica Balacy <jkbalacy@fullsail.edu>
2015-01-29 21:02:08 -0500

    Declared variables
    Declared variables for weight, height, and age.
    Validated each variable with a while loop.

--- SDI_Week4/js/functionsjs.js
+++ SDI_Week4/js/functionsjs.js
@@ -8,3 +8,26 @@
 
 //Calculate the user's Basal Metabolic Rate (BMR) by gathering his/her weight in pounds, height in inches, age, and gender.
 
+//get the user's current weight
+var weight = prompt("What is your current weight in pounds? \nPlease round to the nearest pound.");
+
+//validate the prompt using a while loop
+while(weight==="" || isNaN(weight)) {
+    weight = prompt ("Please enter a numerical value only.");
+}
+
+//get the user's current height
+var height = prompt("What is your current height in inches? \nPlease round to the nearest inch.");
+
+//validate the prompt using a while loop
+while(height==="" || isNaN(height)) {
+    height = prompt ("Please enter a numerical value only.");
+}
+
+//get the user's current age
+var age = prompt("What is your age?");
+
+//validate the prompt using a while loop
+while(age==="" || isNaN(age)) {
+    age = prompt ("Please enter a numerical value only.");
+}
